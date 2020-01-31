655997e6f6c06303b9b8d0633be065c658f52cc4
diptangsu_goswami <diptangsu_goswami@HJD22846>
2020-01-31 16:07:20 +0530

    added templates

--- views/sample.py
+++ views/sample.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from flask import render_template
 
 
 sample_blueprint = Blueprint('sample', __name__)
@@ -7,3 +8,8 @@ sample_blueprint = Blueprint('sample', __name__)
 @sample_blueprint.route('/sample')
 def sample():
     return 'Sample'
+
+
+@sample_blueprint.route('/page')
+def page():
+    return render_template('page.html')
